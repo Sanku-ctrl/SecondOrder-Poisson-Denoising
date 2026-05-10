954443ae31aa0a13e9e721f6786a13b6d7201c3d
Sanket Motagi <se23ucam020@mahindrauniversity.edu.in>
2026-05-10 19:06:31 +0530

    Delete TNRD-Codes/DeblockingTrainingJoint directory

--- TNRD-Codes/DeblockingTrainingJoint/Mapping.m
+++
@@ -1,7 +0,0 @@
-function y = Mapping(x, means, precision, weights)
-x_mu = bsxfun(@minus, x', means);
-% t = bsxfun(@times, x_mu.^2, -0.5*precision);
-% gw = exp(t);
-gw = exp((-0.5*precision)*x_mu.^2);
-q = bsxfun(@times, gw, weights);
-y = sum(q,1)';
